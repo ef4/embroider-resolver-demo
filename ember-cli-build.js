17d251f5a7dbe40697a2def9104cb0b9135654ce
Edward Faulkner <edward@eaf4.com>
2023-05-26 14:09:24 -0400

    stock embroider setup

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { compatBuild } = require('@embroider/compat');
+const { Webpack } = require('@embroider/webpack');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
   });
 
-  return app.toTree();
+  return compatBuild(app, Webpack);
 };
